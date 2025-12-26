f6ccbeec5ec1dba2d6a69e393443f918b6fc6a85
sandstorm791 <sheelyavery@gmail.com>
2025-12-26 18:45:49 -0500

    exception system redone, finished support for -e and -x flags, THERE ARE STILL A COUPLE MINOR BUGS THO

--- fir-common/src/lib.rs
+++ fir-common/src/lib.rs
@@ -1,36 +1,43 @@
-#![no_std]
-use core::{error::Error, fmt};
+#![cfg_attr(not(feature = "user"), no_std)]
+#[cfg(feature = "user")]
+use std::{fmt::{self, Display}};
 
 #[cfg(feature = "user")]
 use aya_obj::generated::{perf_hw_id, perf_sw_ids};
 
 #[cfg(feature = "user")]
 use aya::programs::PerfTypeId;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 
-pub const PERF_EVENT_VARIANTS: usize = 3;
+pub const PERF_EVENT_VARIANTS: usize = 22;
 
+#[cfg(feature = "user")]
 #[derive(Debug)]
-pub enum FlextraceError {
-    TooManyEvents,
-    BadArgument,
-    NoSuchPerfEventType,
-    NoPerfEventCategory,
-    NoPerfHwId,
-    NoPerfSwId,
+pub enum FlextraceError<E = ()> {
+    TooManyEvents(String),
+    BadArgument(String),
+    NoSuchPerfEventType(String),
+    NoPerfEventCategory(String),
+    NoPerfHwId(String),
+    NoPerfSwId(String),
+    Msg(String),
+    Inner(E),
 }
 
-impl fmt::Display for FlextraceError {
+#[cfg(feature = "user")]
+impl<E> fmt::Display for FlextraceError<E> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::TooManyEvents => write!(f, "too many events for one process, max is {PERF_EVENT_VARIANTS}"),
-            Self::BadArgument => write!(f, "bad arguments lol"),
-            Self::NoSuchPerfEventType => write!(f, "the perf event type specified does not exist or is not currently supported"),
+            Self::TooManyEvents(ctx) => write!(f, "too many events for one process, max is {PERF_EVENT_VARIANTS}, context: {ctx}"),
+            Self::BadArgument(ctx) => write!(f, "bad arguments lol, input was {ctx}"),
+            Self::NoSuchPerfEventType(ctx) => write!(f, "the perf event type {ctx} does not exist or is not currently supported"),
             _ => write!(f, "whatever error this is i was too lazy to write an error msg for it"),
         }
     }
 }
 
-impl Error for FlextraceError {}
+#[cfg(feature = "user")]
+impl<E: std::fmt::Debug + Display> std::error::Error for FlextraceError<E> {}
 
 #[derive(Default, Copy, Clone, Debug)]
 #[repr(C)]
@@ -45,7 +52,8 @@ pub struct PerfSample {
 
 // since we want to have one map for all types of perf events we'll use this internally
 // instead of the aya generated perf ids that are category dependent
-#[derive(Default, Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(TryFromPrimitive, IntoPrimitive, Default, Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u8)]
 pub enum PerfEventType {
     #[default]
     None,
@@ -77,54 +85,73 @@ pub enum PerfEventType {
     CgroupSwitches
 }
 
+#[cfg(feature = "user")]
 impl PerfEventType {
-    pub fn from_str(thing: &str) -> Result<PerfEventType, FlextraceError> {
-        match thing {
-            "none" => Ok(PerfEventType::None),
+    pub fn from_str(thing: &String) -> Result<PerfEventType, FlextraceError<String>> {
+        match thing.as_str() {
+            //"none" => Ok(PerfEventType::None),
             "any" => Ok(PerfEventType::Any),
+            "all" => Ok(PerfEventType::Any),
             "cache_miss" => Ok(PerfEventType::CacheMiss),
-            _ => Err(FlextraceError::NoSuchPerfEventType),
+            "page_faults" => Ok(PerfEventType::PageFaults),
+            "cpu_cycles" => Ok(PerfEventType::CpuCycles),
+            "context_switches" => Ok(PerfEventType::ContextSwitches),
+            "cpu_migrations" => Ok(PerfEventType::CpuMigrations),
+            "page_faults_min" => Ok(PerfEventType::PageFaultsMin),
+            "page_faults_maj" => Ok(PerfEventType::PageFaultsMaj),
+            "alignment_faults" => Ok(PerfEventType::AlignmentFaults),
+            "emulation_faults" => Ok(PerfEventType::EmulationFaults),
+            "cgroup_switches" => Ok(PerfEventType::CgroupSwitches),
+            "cpu_clock" => Ok(PerfEventType::CpuClock),
+            "task_clock" => Ok(PerfEventType::TaskClock),
+            "ref_cpu_cycles" => Ok(PerfEventType::RefCpuCycles),
+            "stalled_cycles_backend" => Ok(PerfEventType::StalledCyclesBack),
+            "stalled_cycles_frontend" => Ok(PerfEventType::StalledCyclesFront),
+            "bus_cycles" => Ok(PerfEventType::BusCycles),
+            "branch_misses" => Ok(PerfEventType::BranchMisses),
+            "branch_instructions" => Ok(PerfEventType::BranchInstructions),
+            "cache_references" => Ok(PerfEventType::CacheReferences),
+            "instructions" => Ok(PerfEventType::Instructions),
+            _ => Err(FlextraceError::NoSuchPerfEventType(thing.to_owned())),
         }
     }
 
-    pub fn ebpf_from_self(&self) -> Option<&'static str> {
+    pub fn ebpf_from_self(&self) -> Option<String> {
         match self {
             Self::None => None,
-            Self::Any => Some("generic_perf_handler"),
 
             // hardware
-            Self::CacheMiss => Some("event_cache_miss"),
-            Self::CpuCycles => Some("event_cpu_cycles"),
-            Self::Instructions => Some("event_instructions"),
-            Self::CacheReferences => Some("event_cache_references"),
-            Self::BranchInstructions => Some("event_branch_instructions"),
-            Self::BranchMisses => Some("event_branch_misses"),
-            Self::BusCycles => Some("event_bus_cycles"),
-            Self::StalledCyclesFront => Some("event_stalled_cycles_front"),
-            Self::StalledCyclesBack => Some("event_stalled_cycles_back"),
-            Self::RefCpuCycles => Some("event_ref_cpu_cycles"),
+            Self::CacheMiss => Some(String::from("event_cache_miss")),
+            Self::CpuCycles => Some(String::from("event_cpu_cycles")),
+            Self::Instructions => Some(String::from("event_instructions")),
+            Self::CacheReferences => Some(String::from("event_cache_references")),
+            Self::BranchInstructions => Some(String::from("event_branch_instructions")),
+            Self::BranchMisses => Some(String::from("event_branch_misses")),
+            Self::BusCycles => Some(String::from("event_bus_cycles")),
+            Self::StalledCyclesFront => Some(String::from("event_stalled_cycles_frontend")),
+            Self::StalledCyclesBack => Some(String::from("event_stalled_cycles_backend")),
+            Self::RefCpuCycles => Some(String::from("event_ref_cpu_cycles")),
 
             // software
-            Self::CpuClock => Some("event_cpu_clock"),
-            Self::TaskClock => Some("event_task_clock"),
-            Self::PageFaults => Some("event_page_faults"),
-            Self::ContextSwitches => Some("event_context_switches"),
-            Self::CpuMigrations => Some("event_cpu_migrations"),
-            Self::PageFaultsMaj => Some("event_page_faults_maj"),
-            Self::PageFaultsMin => Some("event_page_faults_min"),
-            Self::AlignmentFaults => Some("event_alignment_faults"),
-            Self::EmulationFaults => Some("event_emulation_faults"),
-            Self::CgroupSwitches => Some("event_cgroup_switches"),
+            Self::CpuClock => Some(String::from("event_cpu_clock")),
+            Self::TaskClock => Some(String::from("event_task_clock")),
+            Self::PageFaults => Some(String::from("event_page_faults")),
+            Self::ContextSwitches => Some(String::from("event_context_switches")),
+            Self::CpuMigrations => Some(String::from("event_cpu_migrations")),
+            Self::PageFaultsMaj => Some(String::from("event_page_faults_maj")),
+            Self::PageFaultsMin => Some(String::from("event_page_faults_min")),
+            Self::AlignmentFaults => Some(String::from("event_alignment_faults")),
+            Self::EmulationFaults => Some(String::from("event_emulation_faults")),
+            Self::CgroupSwitches => Some(String::from("event_cgroup_switches")),
+            _ => None,
         }
     }
 
-    pub fn ebpf_from_str(thing: &str) -> Option<&str> {
-        return PerfEventType::ebpf_from_self(&PerfEventType::from_str(thing).unwrap());
+    pub fn ebpf_from_str(thing: &String) -> Option<String> {
+        return PerfEventType::ebpf_from_self(&PerfEventType::from_str(thing).ok()?);
     }
 
-
-    #[cfg(feature = "user")]
-    pub fn perf_event_category(&self) -> Result<PerfTypeId, FlextraceError> {
+    pub fn perf_event_category(&self) -> Result<PerfTypeId, FlextraceError<String>> {
         match self {
             Self::CacheMiss => Ok(PerfTypeId::Hardware),
             Self::CpuCycles => Ok(PerfTypeId::Hardware),
@@ -148,14 +175,12 @@ impl PerfEventType {
             Self::EmulationFaults => Ok(PerfTypeId::Software),
             Self::CgroupSwitches => Ok(PerfTypeId::Software),
 
-            _ => Err(FlextraceError::NoPerfEventCategory),
+            _ => Err(FlextraceError::NoPerfEventCategory(self.ebpf_from_self().unwrap_or(String::from("")))),
         }
     }
 
-
-    #[cfg(feature = "user")]
     // this function will return an error if the perf event is not a hardware event
-    pub fn perf_hw_id(&self) -> Result<perf_hw_id, FlextraceError> {
+    pub fn perf_hw_id(&self) -> Result<perf_hw_id, FlextraceError<String>> {
         match self {
             Self::CacheMiss => Ok(perf_hw_id::PERF_COUNT_HW_CACHE_MISSES),
             Self::CpuCycles => Ok(perf_hw_id::PERF_COUNT_HW_CPU_CYCLES),
@@ -167,12 +192,11 @@ impl PerfEventType {
             Self::StalledCyclesFront => Ok(perf_hw_id::PERF_COUNT_HW_STALLED_CYCLES_FRONTEND),
             Self::StalledCyclesBack => Ok(perf_hw_id::PERF_COUNT_HW_STALLED_CYCLES_BACKEND),
             Self::RefCpuCycles => Ok(perf_hw_id::PERF_COUNT_HW_REF_CPU_CYCLES),
-            _ => Err(FlextraceError::NoPerfHwId),
+            _ => Err(FlextraceError::NoPerfHwId(self.ebpf_from_self().unwrap_or(String::from("no printable version of this enum??? mysterious...")))),
         }
     }
 
-    #[cfg(feature = "user")]
-    pub fn perf_sw_id(&self) -> Result<perf_sw_ids, FlextraceError> {
+    pub fn perf_sw_id(&self) -> Result<perf_sw_ids, FlextraceError<String>> {
         match self {
             Self::CpuClock => Ok(perf_sw_ids::PERF_COUNT_SW_CPU_CLOCK),
             Self::TaskClock => Ok(perf_sw_ids::PERF_COUNT_SW_TASK_CLOCK),
@@ -184,7 +208,7 @@ impl PerfEventType {
             Self::AlignmentFaults => Ok(perf_sw_ids::PERF_COUNT_SW_ALIGNMENT_FAULTS),
             Self::EmulationFaults => Ok(perf_sw_ids::PERF_COUNT_SW_EMULATION_FAULTS),
             Self::CgroupSwitches => Ok(perf_sw_ids::PERF_COUNT_SW_CGROUP_SWITCHES),
-            _ => Err(FlextraceError::NoPerfSwId),
+            _ => Err(FlextraceError::NoPerfSwId(self.ebpf_from_self().unwrap_or(String::from("")))),
         }
     }
 
--- fir/src/main.rs
+++ fir/src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::Error;
+use aya::programs::perf_attach::PerfLinkId;
+use aya::programs::perf_event::PerfEventLinkId;
 use aya::programs::{PerfEvent, PerfEventScope, PerfTypeId, SamplePolicy};
-use aya::maps::{MapData, RingBuf};
+use aya::maps::{HashMap, MapData, RingBuf};
 use aya::util::online_cpus;
 use clap::{Parser};
-use fir_common::{PerfSample, PerfEventType, PERF_EVENT_VARIANTS};
+use fir_common::{PERF_EVENT_VARIANTS, PerfEventType, PerfSample};
 //#[rustfmt::skip]
 use log::{debug, warn};
 use tokio::io::unix::AsyncFd;
@@ -28,7 +30,7 @@ struct Opt {
     events: Vec<String>,
 
     #[arg(short = 'x', long, value_parser = parse_filter, help = "define events to ignore from certain processes: pid:event1,event2,event3\nor just the pid to drop everything from that process")]
-    filter_exclude: Vec<(String, String)>,
+    filter_exclude: Vec<(u32, [PerfEventType; PERF_EVENT_VARIANTS])>,
 }
 
 // im pretty sure clap automaticlly handles the vec<> part and we
@@ -41,9 +43,9 @@ fn parse_filter(filter: &str) -> anyhow::Result<(u32, [PerfEventType; PERF_EVENT
         let mut to_process = &filter[colon_index + 1..];
 
         while let Some(comma_index) = to_process.find(",") {
-            if events_index >= PERF_EVENT_VARIANTS { return Err(anyhow::Error::msg("too many event arguments for some pid")); }
+            if events_index >= PERF_EVENT_VARIANTS { return Err(anyhow::Error::msg("too many perf events specified yo")); }
             
-            events[events_index] = PerfEventType::from_str(&to_process[..comma_index])?;
+            events[events_index] = PerfEventType::from_str(&to_process[..comma_index].to_string()).unwrap();
 
             if comma_index != events.len() {
                 to_process = &to_process[comma_index + 1..];
@@ -54,11 +56,16 @@ fn parse_filter(filter: &str) -> anyhow::Result<(u32, [PerfEventType; PERF_EVENT
         }
 
         if to_process != "" && events_index >= PERF_EVENT_VARIANTS {
-            return Err(anyhow::Error::msg("too many events for thing"));
+            return Err(anyhow::Error::msg("too many perf events mr white!!!"));
         }
-        events[events_index] = PerfEventType::from_str(&to_process)?;
+        events[events_index] = PerfEventType::from_str(&to_process.to_string()).unwrap();
 
-        Ok((filter[..colon_index].parse()?, events))
+        let key = match filter[..colon_index].parse::<u32>() {
+            Ok(thing) => thing,
+            Err(e) => return Err(anyhow::Error::msg("yo!!! mr white!! can't parse this u32 yo!!!")),
+        };
+
+        Ok((key, events))
     }
     else { Ok((filter.parse()?, [PerfEventType::Any; PERF_EVENT_VARIANTS])) }
 }
@@ -85,7 +92,7 @@ async fn main() -> anyhow::Result<()> {
     }
 
     // include ebpf program at compile time, load at runtime
-    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+    let mut ebpf: aya::Ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/fir"
     )))?;
@@ -99,29 +106,21 @@ async fn main() -> anyhow::Result<()> {
     // forgive me
     for event_arg in opt.events {
         if let Some(event) = PerfEventType::ebpf_from_str(&event_arg) {
-            let perf_event: &mut PerfEvent = ebpf.program_mut(event).unwrap().try_into()?;
+            let perf_event: &mut PerfEvent = ebpf.program_mut(&event).unwrap().try_into()?;
             let perf_event_enum = PerfEventType::from_str(&event_arg)?;
-            let perf_event_category = perf_event_enum.perf_event_category()?;
-
-            let perf_id: u64;
-
-            match perf_event_category {
-                PerfTypeId::Hardware => perf_id = perf_event_enum.perf_hw_id()? as u64,
-                PerfTypeId::Software => perf_id = perf_event_enum.perf_sw_id()? as u64,
-                _ => panic!("please fix this!!! add a handler for perf event types other\nthan hardware and software!!!!!\n\nif you're seeing this in prod i give you full permission to slap me in the face next time you see me"),
-            }
-
-            perf_event.load()?;
             
-            for cpu in online_cpus().map_err(|(_, error)| error)? {
-                perf_event.attach(
-                    perf_event_category.clone(),
-                    perf_id,
-                    PerfEventScope::AllProcessesOneCpu { cpu },
-                    SamplePolicy::Period(1000000),
-                    true,
-                )?; 
+            load_attach_event(perf_event, perf_event_enum)?;
+        }
+        else if PerfEventType::from_str(&event_arg)? == PerfEventType::Any {
+            println!("using all perf events\n");
+
+            for (name, program) in ebpf.programs_mut() {
+                let perf_event: &mut PerfEvent = program.try_into()?;
+                let perf_event_enum = PerfEventType::from_str(&name[6..].to_string())?;
+                
+                load_attach_event(perf_event, perf_event_enum)?;
             }
+            break;
         }
     }
 
@@ -131,6 +130,18 @@ async fn main() -> anyhow::Result<()> {
     let perf_event_buf = RingBuf::try_from(ebpf.take_map("PERF_EVENTS").unwrap()).unwrap();
     let mut asyncfd_perf_buf = AsyncFd::new(perf_event_buf)?;
     let (perf_tx, perf_rx) = mpsc::channel::<PerfSample>();
+
+    let mut filter_exclude_map: HashMap<_, u32, [u8; PERF_EVENT_VARIANTS]> = HashMap::try_from(ebpf.take_map("FILTER_PIDS").unwrap()).unwrap();
+
+    for (key, value) in opt.filter_exclude {
+        let mut value_parsed: [u8; PERF_EVENT_VARIANTS] = [0u8; PERF_EVENT_VARIANTS];
+
+        for i in 0..2 {
+            value_parsed[i] = value[i].into();
+        }
+
+        filter_exclude_map.insert(key, value_parsed, 0)?;
+    }
 
     // poll and read the maps (non-blockingly :D)
     tokio::spawn(async move {
@@ -142,12 +153,16 @@ async fn main() -> anyhow::Result<()> {
     });
 
     loop {
-        let command_str = CStr::from_bytes_until_nul(&perf_rx.recv().unwrap().cmd)
+        let recv = &perf_rx.recv()?;
+
+        let command_str = CStr::from_bytes_until_nul(&recv.cmd)
             .expect("CStr::from_bytes_until_nul failed")
             .to_string_lossy()
             .into_owned();
+
+        let event_type = &recv.event_type.ebpf_from_self().unwrap();
         
-        println!("{command_str}\n");
+        println!("{command_str}\n{event_type}\n");
     }
 
 }
@@ -180,4 +195,37 @@ async fn ringbuf_read<T: Copy>(fd: &mut AsyncFd<RingBuf<MapData>>) -> Result<Vec
 
         readguard.clear_ready();
         Ok(items)
+}
+
+fn load_attach_event(perf_event: &mut PerfEvent, perf_event_enum: PerfEventType) -> anyhow::Result<Vec<PerfEventLinkId>> {
+    let perf_event_category = perf_event_enum.perf_event_category()?;
+    let perf_id: u64;
+
+    match perf_event_category {
+        PerfTypeId::Hardware => perf_id = perf_event_enum.perf_hw_id()? as u64,
+        PerfTypeId::Software => perf_id = perf_event_enum.perf_sw_id()? as u64,
+        _ => panic!("please fix this!!! add a handler for perf event types other\nthan hardware and software!!!!!\n\nif you're seeing this in prod i give you full permission to slap me in the face next time you see me"),
+    }
+
+    perf_event.load()?;
+
+    let mut links: Vec<PerfEventLinkId> = Vec::new();
+
+    for cpu in online_cpus().map_err(|(_, error)| error)? {
+            match perf_event.attach(
+                perf_event_category.clone(),
+                perf_id,
+                PerfEventScope::AllProcessesOneCpu { cpu },
+                SamplePolicy::Period(1000000),
+                true,
+            ) {
+                Ok(link_id) => links.push(link_id),
+                Err(e) => {
+                    println!("system does not support perf event {}", perf_event_enum.ebpf_from_self().unwrap_or(String::from("could not get the string version of this event i guess, weird...")));
+                    break;
+                },
+            };
+    }
+
+    Ok(links)
 }
